2c8c72ea5946f665701d8c233b27638b41b9f2dd
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-16 17:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
-  returns (kittens)
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
-  returns (kittens)
 }
 
 function destructivelyRemoveLastKitten() {
   kittens.pop()
-  returns (kittens)
 }
 
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
-  returns (kittens)
 }
 
 function appendKitten(name) {
