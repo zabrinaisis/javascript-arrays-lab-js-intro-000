c8f47fd4011a2573fc216716dbdc04f8e18bee7e
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-16 17:45:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   returns (kittens)
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  returns (kittens)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  returns (kittens)
+}
+
+function appendKitten(name) {
+  var newKittens = [name, ...kittens]
+  return (newKittens)
+}
