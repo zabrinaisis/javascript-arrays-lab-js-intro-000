abdb1296ecc5e9d3da6f7eafe706b8178ee837a9
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-16 17:48:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,8 @@ function prependKitten(name) {
   var newKittens = [name, ...kittens]
   return (newKittens)
 }
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, (kittens.length-1))
+  return (newKittens)
+}
